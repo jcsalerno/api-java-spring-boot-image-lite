0a7bf54b9e3cb651f882cfb511150e51ed2e22ed
Julio Cesar <contato.jcsalerno@gmail.com>
2024-04-28 22:55:39 -0300

    feat: Front-end beginner search screen improvements

--- imagelite/src/components/ImageCard.tsx
+++ imagelite/src/components/ImageCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 interface ImageCardProps {
   nome?: string;
   tamanho?: string;
@@ -11,9 +12,18 @@ export const ImageCard: React.FC<ImageCardProps> = ({
   src,
   tamanho,
 }: ImageCardProps) => {
+  function download() {
+    window.open(src, "_blank");
+  }
+
   return (
     <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:-transalte-y-2">
-      <img src={src} className="h-56 w-full object-cover rounded-t-md" alt="" />
+      <img
+        src={src}
+        onClick={download}
+        className="h-56 w-full object-cover rounded-t-md"
+        alt=""
+      />
       <div className="card-body p-4">
         <h5 className="text-x1 font-semibold mb-2 text-gray-600">{nome}</h5>
         <p className="text-gray-600">{tamanho}</p>
